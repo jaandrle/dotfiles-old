566c8d473506ce4dbff7ffe64cda4ed14ae8c1e5
Jan Andrle <andrle.jan@centrum.cz>
2020-05-31 21:55:15 +0200

    Update rofi-locate.sh

--- ubuntu/.run/rofi-locate.sh
+++ ubuntu/.run/rofi-locate.sh
@@ -1,9 +1,13 @@
-[ $# -eq 0 ] && ls -a -t -d /media/jaandrle/*/ ~ ~/* ~/*/* ~/*/*/* |\
-    grep -v "/home/jaandrle/\(PlayOnLinux\|Šablony\|Dokumenty/NHL09\|snap\)" |\
-    sed -e 's:/home/jaandrle:~:'
-if [ $# -eq 1 ]
+if [ $# != 1 ]
 then
-    cmd=$(echo "$1" | sed -e 's:~:/home/jaandrle:')
-    xdg-open "$cmd" >/dev/null 2>&1 &
-    exit 0
+    ls -a -t -d /media/jaandrle/*/ ~ ~/* ~/*/* ~/*/*/* |\
+        grep -v "$HOME/\(PlayOnLinux\|Šablony\|Dokumenty/NHL09\|snap\)" |\
+        sed -e "s:$HOME:~:"
+    exit
 fi
+case "$1" in !*)
+    nohup catfish "${1#"!"}" >/dev/null 2>&1 &
+    exit
+esac
+cmd=$(echo "$1" | sed -e 's:~:/home/jaandrle:')
+xdg-open "$cmd" >/dev/null 2>&1 &
