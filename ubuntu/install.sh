bd5d72e3d65b7404b0dd427a355955daa48b410c
Jan Andrle <andrle.jan@centrum.cz>
2021-12-01 13:26:59 +0100

    :boom: Update “all” my software utility

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -8,5 +8,5 @@ ln -s $this_dir/bin/_cordova-release.mjs ~/bin/_cordova-release.mjs
 ln -s $this_dir/bin/_extract ~/bin/_extract
 ln -s $this_dir/bin/_vim_plugins ~/bin/_vim_plugins
 ln -s $this_dir/bin/_weather ~/bin/_weather
-ln -s $this_dir/bin/github-releases.js ~/bin/github-releases.js
+cp -i $this_dir/bin/github-releases.js ~/bin/github-releases.js
 cp -i $this_dir/bin/github-releases.json ~/bin/github-releases.json
