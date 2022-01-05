18f2ce912235920a08ad03d79b420bd261292fa2
Jan Andrle <andrle.jan@centrum.cz>
2022-01-05 18:26:20 +0100

    :tada: Vim Snippets Sync

--- vim/install.sh
+++ vim/install.sh
@@ -3,3 +3,5 @@ this_dir=$(dirname $(readlink -f $0))
 ln -s $this_dir/.vimrc ~/.vimrc
 ln -s $this_dir/.vim/coc-settings.json ~/.vim/coc-settings.json
 ln -s $this_dir/.vim/intro-template.md ~/.vim/intro-template.md
+
+ln -s $this_dir/ultisnips ~/.config/coc/ultisnips
