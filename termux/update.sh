e0e71aa41def8e8873e1217984ad7ee1b8be814d
Jan Andrle <andrle.jan@centrum.cz>
2022-07-22 12:15:20 +0200

    :boom:

---
+++ termux/update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+cd ~
+local url_root=https://raw.githubusercontent.com/jaandrle/dotfiles/master
+curl $url_root/termux/.vimrc --output ~/.vimrc
+curl $url_root/bash/.inputrc --output ~/.inputrc
+mkdir ~/.newsboat
+curl $url_root/ubuntu/.newsboat/config --output ~/.newsboat/config
+curl $url_root/ubuntu/.newsboat/urls --output ~/.newsboat/urls
+mkdir ~/bin
+ln -s $PREFIX/bin/vim ~/bin/termux-file-editor
+# termux-setup-storage
